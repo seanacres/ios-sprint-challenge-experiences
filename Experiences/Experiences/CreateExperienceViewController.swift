45510832924548d3bcab017eb5e272ee86588545
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-18 13:12:29 -0400

    Implemented audio recording and saving

--- Experiences/Experiences/CreateExperienceViewController.swift
+++ Experiences/Experiences/CreateExperienceViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import AVFoundation
 
 class CreateExperienceViewController: UIViewController {
 
@@ -16,6 +17,12 @@ class CreateExperienceViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var recordAudioButton: UIButton!
+    
+    weak var timer: Timer?
+    
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
+    
     
     var originalImage: UIImage? {
         didSet {
@@ -53,6 +60,114 @@ class CreateExperienceViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func updateViews() {
+        recordAudioButton.isEnabled = isRecording
+        recordAudioButton.isSelected = isRecording
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    // MARK: - Timer
+
+    func startTimer() {
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.030, repeats: true) { [weak self] (_) in
+            guard let self = self else { return }
+            
+            self.updateViews()
+        }
+    }
+    
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    // MARK: - Recording
+    
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
+    func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+        
+        print("recording URL: \(file)")
+        
+        return file
+    }
+    
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: []) // can fail if on a phone call, for instance
+    }
+    
+    func requestPermissionOrStartRecording() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                guard granted == true else {
+                    print("We need microphone access")
+                    return
+                }
+                
+                print("Recording permission has been granted!")
+                // NOTE: Invite the user to tap record again, since we just interrupted them, and they may not have been ready to record
+            }
+        case .denied:
+            print("Microphone access has been blocked.")
+            
+            let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
+        }
+    }
+    
+    func startRecording() {
+        do {
+            try prepareAudioSession()
+        } catch {
+            print("Cannot record audio \(error)")
+            return
+        }
+        
+        recordingURL = createNewRecordingURL()
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: recordingURL!, format: format)
+            audioRecorder?.delegate = self
+            audioRecorder?.record()
+            updateViews()
+            startTimer()
+        } catch {
+            preconditionFailure("Audio recorder could not be created with \(recordingURL!) and format: \(format) and error: \(error)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        updateViews()
+        cancelTimer()
     }
     
     // MARK: - Photo
@@ -107,9 +222,17 @@ class CreateExperienceViewController: UIViewController {
     }
     
     @IBAction func recordAudioTapped(_ sender: Any) {
+        if isRecording {
+            stopRecording()
+        } else {
+            requestPermissionOrStartRecording()
+        }
     }
     
     @IBAction func saveTapped(_ sender: Any) {
+        guard let titleText = experienceTitleTextField.text else { return }
+        
+        let experience = Experience(title: titleText, image: originalImage, audioRecording: nil, latitude: 0, longitude: 0)
     }
 }
 
@@ -126,5 +249,18 @@ extension CreateExperienceViewController: UIImagePickerControllerDelegate, UINav
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreateExperienceViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        audioRecorder = nil
+        cancelTimer()
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            print("error: \(error)")
+        }
     }
 }
