d1800d47d5adf4da5eb69fe1029145307dafd024
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-18 12:04:43 -0400

    Initial project setup

---
+++ Experiences/Experiences/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Experiences
+//
+//  Created by Sean Acres on 7/18/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
