1e21728b530021ad07128e41e06cfdc6dd932db4
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-18 12:37:58 -0400

    Added presentation of image picker

--- Experiences/Experiences/CreateExperienceViewController.swift
+++ Experiences/Experiences/CreateExperienceViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 class CreateExperienceViewController: UIViewController {
 
@@ -15,11 +17,27 @@ class CreateExperienceViewController: UIViewController {
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var recordAudioButton: UIButton!
     
+    var originalImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Photo
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("Photo Library not available")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
 
@@ -34,11 +52,28 @@ class CreateExperienceViewController: UIViewController {
     */
     
     @IBAction func addPhotoTapped(_ sender: Any) {
+        presentImagePickerController()
     }
     
     @IBAction func recordAudioTapped(_ sender: Any) {
     }
     
     @IBAction func saveTapped(_ sender: Any) {
+    }
+}
+
+extension CreateExperienceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
